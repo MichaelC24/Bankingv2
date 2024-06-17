29f3479054e36eafa4c7a0339a1ca849b1f48f26
Michael Clemons <Mclemons@tql.com>
2024-06-17 14:14:28 -0400

    Add project files.

---
+++ Bankingv2/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankingv2
+{
+    public class Account
+    {
+        public int AccountId { get; set; } = 0;
+        public string Description { get; set; } = string.Empty;
+        public decimal Balance { get; set; } = 0;
+
+        public bool Deposit(decimal deposit)
+        {
+            if (deposit > 0)
+            {
+                Balance += deposit;
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool Withdraw(decimal withdraw)
+        {
+            if (withdraw <= Balance)
+            {
+                Balance -= withdraw;
+                return true;
+
+            } return false;
+        }
+        //account number needs to be same type as the variable you pass to it
+        public bool Transfer(decimal Amount, Account account)
+        {
+            var success = Withdraw(Amount);
+
+            if (success) 
+            {
+               account.Deposit(Amount);
+            }
+
+            return true;
+        }
+
+        
+    }
+}
+
---
+++ Bankingv2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.AccessControl;
+
+namespace Bankingv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account();
+            Account a2 = new Account();
+
+            Console.WriteLine($"Your balance is {a1.Balance:C} ");
+            a1.Deposit(1000);
+            Console.WriteLine($"Your current balance is {a1.Balance:C}");
+            a1.Withdraw(100);
+            Console.WriteLine($"Your current balance is {a1.Balance:C}");
+
+            a1.Transfer(100,a2);
+            Console.WriteLine($"A2 Balance {a2.Balance:C}");
+        }
+    }
+}
